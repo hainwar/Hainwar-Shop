74795c348e76576b02fb62801274c7a63b5af7e6
HAIRUL ANWAR <136827134+hainwar@users.noreply.github.com>
2024-12-13 05:51:08 +0000

    Hainwar shop

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,12 @@ document.querySelectorAll('.nav-links a').forEach(link => {
         }
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
